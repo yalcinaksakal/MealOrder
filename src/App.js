128a090b6e173d22c083d93321c3cff8d11bc6f8
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-17 18:21:05 +0300

    meals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [isCartShown, setIsCartShown] = useState(false);
@@ -9,13 +10,13 @@ function App() {
   const showCartHandler = () => setIsCartShown(true);
   const hideCartHandler = () => setIsCartShown(false);
   return (
-    <>
+    <CartProvider>
       {isCartShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/Cart/ButtonCart.js
+++ src/components/Cart/ButtonCart.js
@@ -1,13 +1,17 @@
 import styles from "./ButtonCart.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+
 const ButtonCart = props => {
+  const ctx = useContext(CartContext);
   return (
     <button className={styles.button} onClick={props.clicked}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{ctx.items.length}</span>
     </button>
   );
 };
