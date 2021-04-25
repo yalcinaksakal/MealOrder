bc40f137a983a38e54810968cbb3fa238adb5445
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-26 00:04:31 +0300

    mealOrder

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,32 +4,32 @@ import Card from "../UI/Card";
 import { useEffect, useState } from "react";
 import Spinner from "../UI/Spinner";
 const AvailableMeals = () => {
+  //mealsList= spinner,error or meals list
   const [mealsList, setMealsList] = useState(<Spinner />);
 
   useEffect(() => {
     const fetchMeals = async () => {
-      try {
-        const response = await fetch(
-          "https://order-meal-a2f7a-default-rtdb.firebaseio.com/meals/-MZ9QVLnro9jipm5VE1u.json"
-        );
-        console.log(response);
-        const data = await response.json();
-        setMealsList(
-          <section className={styles.meals}>
-            <Card>
-              <ul>
-                {Object.values(data).map(meal => (
-                  <MealItem key={meal.id} meal={meal} />
-                ))}
-              </ul>
-            </Card>
-          </section>
-        );
-      } catch (error) {
-        setMealsList(<p>{error.message}</p>);
-      }
+      const response = await fetch(
+        "https://order-meal-a2f7a-default-rtdb.firebaseio.com/meals/-MZ9QVLnro9jipm5VE1u.json"
+      );
+      const data = await response.json();
+      if (!data) throw new Error("Couldn't get meals from server.");
+      setMealsList(
+        <section className={styles.meals}>
+          <Card>
+            <ul>
+              {Object.values(data).map(meal => (
+                <MealItem key={meal.id} meal={meal} />
+              ))}
+            </ul>
+          </Card>
+        </section>
+      );
     };
-    fetchMeals();
+
+    fetchMeals().catch(error => {
+      setMealsList(<p className={styles.error}>{error.message}</p>);
+    });
   }, []);
   return mealsList;
 };
