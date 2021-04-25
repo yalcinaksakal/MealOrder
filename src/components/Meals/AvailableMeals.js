c39fe97532cf915be7ee285db047dacca5e157ef
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-25 23:28:48 +0300

    mealOrder

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,18 +1,32 @@
 import styles from "./AvailableMeals.module.css";
-import products from "../../assests/products/products";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
+import { useEffect, useState } from "react";
+import Spinner from "../UI/Spinner";
 const AvailableMeals = () => {
-  const mealsList = products.map((meal, i) => {
-    meal.id = "m" + i;
-    return <MealItem key={meal.id} meal={meal} />;
-  });
+  const [mealsList, setMealsList] = useState(<Spinner />);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://order-meal-a2f7a-default-rtdb.firebaseio.com/meals/-MZ9QVLnro9jipm5VE1u.json"
+      );
+      const data = await response.json();
+      setMealsList(
+        <ul>
+          {Object.values(data).map(meal => (
+            <MealItem key={meal.id} meal={meal} />
+          ))}
+        </ul>
+      );
+    };
+    fetchMeals();
+  }, []);
   return (
     <section className={styles.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
+      <Card>{mealsList}</Card>
     </section>
   );
 };
+
 export default AvailableMeals;
